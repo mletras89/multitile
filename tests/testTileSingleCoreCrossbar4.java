28a76f68f69f2bacb72c8ff0521fa0733c27640c
Martin Letras <martin.letras@fau.de>
2023-03-14 09:04:58 +0100

    removing unused headers from classes

--- tests/testTileSingleCoreCrossbar4.java
+++ tests/testTileSingleCoreCrossbar4.java
@@ -40,24 +40,10 @@ import multitile.scheduler.FCFS;
 
 import multitile.architecture.Architecture;
 import multitile.architecture.Tile;
-import multitile.architecture.Memory;
-import multitile.architecture.Processor;
 
 import multitile.application.Application;
-import multitile.application.Actor;
-import multitile.application.Fifo;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 public class testTileSingleCoreCrossbar4{
     public static void main(String[] args) throws IOException {
--- tests/testWriteReadTransfers.java
+++ tests/testWriteReadTransfers.java
@@ -48,16 +48,7 @@ import multitile.application.Actor;
 import multitile.application.Fifo;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 public class testWriteReadTransfers {
     public static void main(String[] args) throws IOException {
