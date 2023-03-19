3a9ca2fd061eeb7aa6acfbef0d290a52f918f237
mletras89 <martysan11@gmail.com>
2023-03-19 10:19:01 +0100

    updating the mappings

--- mapping/Mapping.java
+++ mapping/Mapping.java
@@ -1,1 +1,78 @@
-java
+/*
+--------------------------------------------------------------------------
+ Copyright (c) 2022 Hardware-Software-Co-Design, Friedrich-
+ Alexander-Universitaet Erlangen-Nuernberg (FAU), Germany. 
+ All rights reserved.
+ 
+ This code and any associated documentation is provided "as is"
+ 
+ IN NO EVENT SHALL HARDWARE-SOFTWARE-CO-DESIGN, FRIEDRICH-ALEXANDER-
+ UNIVERSITAET ERLANGEN-NUERNBERG (FAU) BE LIABLE TO ANY PARTY FOR DIRECT,
+ INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ OF THE USE OF THIS CODE AND ITS DOCUMENTATION, EVEN IF HARDWARE-
+ SOFTWARE-CO-DESIGN, FRIEDRICH-ALEXANDER-UNIVERSITAET ERLANGEN-NUERNBERG
+ (FAU) HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THE
+ AFOREMENTIONED EXCLUSIONS OF LIABILITY DO NOT APPLY IN CASE OF INTENT
+ BY HARDWARE-SOFTWARE-CO-DESIGN, FRIEDRICH-ALEXANDER-UNIVERSITAET
+ ERLANGEN-NUERNBERG (FAU).
+ 
+ HARDWARE-SOFTWARE-CO-DESIGN, FRIEDRICH-ALEXANDER-UNIVERSITAET ERLANGEN-
+ NUERNBERG (FAU), SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT
+ NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE.
+ 
+ THE CODE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND HARDWARE-
+ SOFTWARE-CO-DESIGN, FRIEDRICH-ALEXANDER-UNIVERSITAET ERLANGEN-
+ NUERNBERG (FAU) HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+ UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ -------------------------------------------------------------------------
+ 
+  @author Martin Letras
+  @date   19 March 2023
+  @version 1.1
+  @ brief
+     Mapping T(Actor or fifo) to U(processor or memory)
+--------------------------------------------------------------------------
+*/
+package multitile.application;
+
+import java.util.*;
+
+public class Mapping<T,U>{
+  private T source;
+  private U destination;
+  private HashMap<String,Object> properties;
+  
+  public Mapping(){
+    source = null;
+    destination = null;
+    properties = new HashMap<String,Object>();
+  }
+
+  public Mapping(T source, U destination){
+    this.source = source;
+    this.destination = destination;
+    properties = new HashMap<String,Object>();
+  }
+
+  public T getSource(){
+    return source;
+  }
+
+  public void setSource(T source){
+    this.source = source;
+  }
+
+  public U getDestination(){
+    return destination;
+  }
+
+  public void setDestination(U destination){
+    this.destination = destination;
+  }
+
+  public HashMap<String,Object> getProperties(){
+    return properties;
+  }
+
+}
