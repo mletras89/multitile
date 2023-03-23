472fb670c581cc583dce02220bb2b971f38f3ce5
Martin Letras <martin.letras@fau.de>
2023-03-23 18:03:11 +0100

    removing redundant code

--- application/ApplicationManagement.java
+++ application/ApplicationManagement.java
@@ -40,6 +40,7 @@ package multitile.application;
 
 import java.util.*;
 
+import integration.MyEntry;
 import multitile.architecture.Architecture;
 import multitile.architecture.Tile;
 import multitile.mapping.Binding;
@@ -383,45 +384,14 @@ public class ApplicationManagement{
 	    
 	    for(Map.Entry<Integer,Actor> multicastActor : multicastActors.entrySet()){
 	      Actor selectedActor = multicastActor.getValue();
-	      
 	      collapseMergeableMulticastActor(app,selectedActor,startIndex++);
-	      /*if(selectedActor.isMergeMulticast() == true){
-	        // if the actor is mergeable, we remove it and replace it by a composite channel
-	        Vector<Fifo> inputFifos  = selectedActor.getInputFifos(); // it should be only one writer
-	        Vector<Fifo> outputFifos = selectedActor.getOutputFifos(); // it might be multiple readers, more that one
-	        Fifo writer = inputFifos.get(0);
-	        List<Fifo>  readerFifos = new ArrayList<Fifo>(outputFifos);
-
-	        CompositeFifo compositeFifo = FifoManagement.createCompositeChannel(writer,readerFifos,selectedActor,startIndex++); 
-	        // once created the compositefifo, we have to connected into the application
-	        int idWriterActor = writer.getSource().getId();
-	        app.getActors().get(idWriterActor).removeOutputFifo(writer.getId());
-	        // connecting the input of the composite fifo
-	        app.getActors().get(idWriterActor).getOutputFifos().add(compositeFifo);
-
-	        // now connect the readers to the composite fifo
-	        for(Fifo dstFifo : readerFifos){
-	          int idReaderActor = dstFifo.getDestination().getId();
-	          app.getActors().get(idReaderActor).removeInputFifo(dstFifo.getId());
-	          // connectinf the outputs of the composite fifo
-	          app.getActors().get(idReaderActor).getInputFifos().add(compositeFifo);
-	        }
-	        // remove the fifos
-	        app.getFifos().remove(writer.getId());
-	        for(Fifo dstFifo : readerFifos) {
-	        	app.getFifos().remove(dstFifo.getId());
-	        }
-	        // remove the merged multicast actor from the map of actors
-	        app.getActors().remove(multicastActor.getKey());
-	        // add the new composite fifo into the app fifo map
-	        app.getFifos().put(compositeFifo.getId(),compositeFifo);
-	      }*/
 	    }
   }
 
-  public static Fifo collapseMergeableMulticastActor(Application app, Actor multicastActor, int index){
+  public static MyEntry<Fifo, CompositeFifo> collapseMergeableMulticastActor(Application app, Actor multicastActor, int index){
 	  // returns the writer fifo
 	  Fifo writer=null;
+	  CompositeFifo compositeFifo = null;
 	  if(multicastActor.isMergeMulticast() == true){
         // if the actor is mergeable, we remove it and replace it by a composite channel
         Vector<Fifo> inputFifos  = multicastActor.getInputFifos(); // it should be only one writer
@@ -429,7 +399,7 @@ public class ApplicationManagement{
         writer = inputFifos.get(0);
         List<Fifo>  readerFifos = new ArrayList<Fifo>(outputFifos);
 
-        CompositeFifo compositeFifo = FifoManagement.createCompositeChannel(writer,readerFifos,multicastActor,index); 
+        compositeFifo = FifoManagement.createCompositeChannel(writer,readerFifos,multicastActor,index); 
         // once created the compositefifo, we have to connected into the application
         int idWriterActor = writer.getSource().getId();
         app.getActors().get(idWriterActor).removeOutputFifo(writer.getId());
@@ -453,7 +423,9 @@ public class ApplicationManagement{
         // add the new composite fifo into the app fifo map
         app.getFifos().put(compositeFifo.getId(),compositeFifo);
       }
-	  return writer;
+	  //return writer;
+	  MyEntry<Fifo, CompositeFifo> result = new MyEntry<Fifo,CompositeFifo>(writer,compositeFifo);
+	  return result;
   }
   
 }
