119d4328f16405ae16a1ed88d8bb9844a7376bfb
Martin Letras <martin.letras@fau.de>
2023-04-14 14:48:49 +0200

    the calculation of time is in seconds, no more in microseconds

--- architecture/NoC.java
+++ architecture/NoC.java
@@ -259,7 +259,7 @@ public class NoC{
   
   public double calculateTransferTime(Transfer transfer){
     int numberofBytes = transfer.getBytes();
-    double processingTime = ((( BytesToGigabytes(numberofBytes) / this.bandwidthPerChannel))*1000000); // 8 bits in a byte, 100 000 to convert from secs to microseconds
+    double processingTime = ((( BytesToGigabytes(numberofBytes) / this.bandwidthPerChannel))); // 8 bits in a byte, 100 000 to convert from secs to microseconds
     return processingTime;
   }
 
